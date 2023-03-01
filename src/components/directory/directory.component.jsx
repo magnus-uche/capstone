dc4023000e87426866cfb8a9113639accff34dd1
ukjunior <devmagnusuche@gmail.com>
2023-03-01 06:45:45 -0800

    first commit

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,14 @@
+import CategoryItem from "../category-item/category-item.component";
+import './directory.styles.scss';
+
+const Directory = ({ category }) => {
+  return (
+    <div className="directory-container">
+      {category.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+}; 
+
+export default Directory;
